0b4ab062d444c9b178044f34f668f985afbd3152
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2026-01-09 23:39:43 +0600

    Initial Commit

---
+++ PeopleHub/PeopleHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleHubApp.swift
+//  PeopleHub
+//
+//  Created by Hafizur Rahman on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct PeopleHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
